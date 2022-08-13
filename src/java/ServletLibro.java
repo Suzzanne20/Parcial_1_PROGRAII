3d8830c00d12eb463ca6c66eccadf6e20466a267
Suzzanne20 <susanacevedo11@hotmail.com>
2022-08-13 10:36:19 -0600

    finalizacion servlet

--- src/java/ServletLibro.java
+++ src/java/ServletLibro.java
@@ -2,7 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
+import Clases.Libro;
+import Clases.LibroVec;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -17,7 +18,9 @@ import jakarta.servlet.http.HttpServletResponse;
  */
 @WebServlet(urlPatterns = {"/ServletLibro"})
 public class ServletLibro extends HttpServlet {
-
+    Libro libro;
+    LibroVec regLibro;
+    Libro[] registro;
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -32,29 +35,52 @@ public class ServletLibro extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try ( PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
+            libro=new Libro(
+            request.getParameter("cod"),
+            request.getParameter("name"),
+            request.getParameter("type"),
+            request.getParameter("edi"),
+            request.getParameter("age"));
+        if(regLibro==null){regLibro=new LibroVec();}
+        regLibro.saveLi(libro);  registro=regLibro.gimmeLi();
+          
+        
             out.println("<!DOCTYPE html>");
             out.println("<html>");
-            out.println("    <head>\n" +
-            "        <title>Registros</title>\n" +
-            "        <meta charset=\"UTF-8\">\n" +
-            "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-            "        <link rel=\"shortcut icon\" href=\"https://static.vecteezy.com/system/resources/previews/001/200/145/non_2x/books-png.png\" type=\"image/x-icon\"/>\n" +
-            "        <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css\" >\n" +
-            "        <link href=\"estiloPrinc.css\" rel=\"stylesheet\" type=\"text/css\"/>\n" +
-            "        <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct\" crossorigin=\"anonymous\"></script>\n" +
-            "        <nav class=\"navbar navbar-dark bg-dark\">\n" +
+            out.println("<head>");
+            out.println("<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css' >");
+            out.println("<script src='https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js' ></script>"+
+            "          <link rel=\"shortcut icon\" href=\"https://raw.githubusercontent.com/Suzzanne20/ResourceNekoStation/main/Neko-icon.ico\" type=\"image/x-icon\"/>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<nav class=\"navbar navbar-dark bg-dark\">\n" +
             "          <div class=\"container-fluid row text-center \"> <div class=\"row align-items-center\">\n" +
             "          <a class=\"navbar-brand col\" href=\"index.html\">\n" +
             "          <img src=\"https://static.vecteezy.com/system/resources/previews/001/200/145/non_2x/books-png.png\" alt=\"\" width=\"30\" height=\"30\" class=\"d-inline-block align-text-top\">Biblioteca</a>\n" +
             "          <a class=\"navbar-brand col \" href=\"ServletLibro\">Libros</a>\n" +
             "          <a class=\"navbar-brand col \" href=\"page2.html\">Registro</a>\n" +
-            "          </div></div>\n" +
-            "        </nav>\n" +
-            "    </head>");
-            out.println("<body>");
-            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            "          </div></div></nav>");
+            
+            out.println("<main class=\"main \"><div class=\"container text-center \"><br><h1>Clientes Registrados</h1><br><table class=\"table table-dark table-hover\"><thead><tr>\n" +
+            "      <th scope=\"col\">COD</th>\n" +
+            "      <th scope=\"col\">NOMBRE</th>\n" +
+            "      <th scope=\"col\">TIPO</th>\n" +
+            "      <th scope=\"col\">EDICION</th>\n" +
+            "      <th scope=\"col\">AÑO</th>\n" +
+            "      </tr></thead><tbody>");
+            for (int i=0; i<registro.length;i++){
+                if(!registro[i].getCodigo().isEmpty()){
+                out.println("    <tr>\n" +
+            "      <th scope=\"row\">"+ registro[i].getCodigo()+"</th>\n" +
+            "      <td>"+registro[i].getNombre() +"</td>\n" +
+            "      <td>"+registro[i].getTipo() +"</td>\n" +
+            "      <td>"+registro[i].getEditorial()+"</td>\n" +
+            "      <td>"+registro[i].getAño()+"</td></tr>");
+                
+                }
+            }                     
+            out.println("</tbody></table></div>");
+            out.println("</main></body></html>");
         }
     }
 
